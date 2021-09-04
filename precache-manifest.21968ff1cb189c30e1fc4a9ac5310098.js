216d822005d27017dd32cafbc2be44a2679e5f39
marecek1337 <63104254+marecek1337@users.noreply.github.com>
2021-09-04 13:00:00 +0200

    Updates

---
+++ precache-manifest.21968ff1cb189c30e1fc4a9ac5310098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7254112905ffefa28862563f053c1dc",
+    "url": "/marekgram/index.html"
+  },
+  {
+    "revision": "68cd8b45839a45d3987d",
+    "url": "/marekgram/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "916f7bcd58076d264206",
+    "url": "/marekgram/static/js/2.5871728b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/marekgram/static/js/2.5871728b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68cd8b45839a45d3987d",
+    "url": "/marekgram/static/js/main.7c2b1198.chunk.js"
+  },
+  {
+    "revision": "6d277dc281c8f2c915d9",
+    "url": "/marekgram/static/js/runtime-main.20d568be.js"
+  }
+]);
